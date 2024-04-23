3743ea81af42f801d502d48ea6be98d5914fa7cf
Danilo Montecel <daniloomarmontecel@gmail.com>
2024-04-23 07:54:43 -0500

    cambio 2

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,8 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../utils/connection');
-
-const User = sequelize.define('modelName', {
+const bcrypt=require('bcrypt');
+const { NOEXPAND } = require('sequelize/lib/table-hints');
+const User = sequelize.define('user', {
     firstName: {
         type: DataTypes.STRING,
         allowNull: false
@@ -26,5 +27,22 @@ const User = sequelize.define('modelName', {
     },
 
 });
+/**Esto es un hooks que es llamado por el
+ * controlador create para encriptar el password
+ * atravez de bcrypt y asignarlo a la propiedad
+ * Ademas se emplea en el metodo beforeCreate 
+ * que es de sequlize 
+ */
+User.beforeCreate(async(user)=>{
+    const password=user.password
+    const hashePassword=await bcrypt.hash(password, 10)
+    user.password=hashePassword
+})
+User.prototype.toJSON=function(){
+    const values={...this.get()};
+    delete values.password;
+    return values;
+}
+
 
 module.exports = User;
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,15 +1,15 @@
-const { getAll, create, getOne, remove, update } = require('../controllers/controllerFile');
+const { getAll, create, getOne, remove, update } = require('../controllers/user.controllers');
 const express = require('express');
 
-const routerUser = express.Router();
+const routerName = express.Router();
 
-routerUser.route('/')
+routerName.route('/')
     .get(getAll)
     .post(create);
 
-routerUser.route('/:id')
+routerName.route('/:id')
     .get(getOne)
     .delete(remove)
     .put(update);
 
-module.exports = routerUser;
+module.exports = routerName;
